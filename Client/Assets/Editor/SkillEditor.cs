268a744ffd4f4dc83aa2e3aa3280c25a7b1bb02e
dgzhaochuan <243686023@qq.com>
2017-12-15 23:54:56 +0800

    1

---
+++ Client/Assets/Editor/SkillEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class SkillEditor : EditorWindow
+{
+
+    [MenuItem("Tools/SkillWindow")]
+    static void window()
+    {
+        //创建窗口
+        Rect wr = new Rect(0, 0, 500, 500);
+        SkillEditor window = (SkillEditor)EditorWindow.GetWindow(typeof(SkillEditor));
+        window.Show();
+    }
+}
